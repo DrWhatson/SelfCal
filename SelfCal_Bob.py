c3573742d22ad88044c701574bf28ca5f2da65c0
Bob Watson <bob.watson@manchester.ac.uk>
2020-06-25 08:08:41 +0100

    Backup before changing code

--- SelfCal_Bob.py
+++ SelfCal_Bob.py
@@ -5,7 +5,7 @@ from numbers import Number
 
 tmpvis_default = 'Shift.ms'
 
-wsclean_version = 'wsclean-2.8'
+wsclean_version = 'wsclean'
 wsclean_defaults = {'j':None,
                     'field':None,
                     'data-column':'DATA',
@@ -23,14 +23,31 @@ wsclean_defaults = {'j':None,
 
 cal_parms_default = {'cal_dir':'calibration',
                      'callib':'callib.txt',
-                     'prefix':'gcal.',
+                     'table':'gcal.0',
                      'solint':'inf',
                      'gaintype':'G',
                      'calmode':'p',
                      'minsnr': 2.5}
 
-outvis_default = {'outvis':'SelfCald.ms'}
+split_default = {'outvis':'SelfCald.ms',
+                 'script':'split.py'}
 
+pipeline_default = {'cycles': 1,
+                    'copy': False,
+                    'chgcentre_src': False,
+                    'wsclean0': False,
+                    'gain0': False,
+                    'wsclean1': False,
+                    'gain1': False,
+                    'wsclean2': False,
+                    'gain2': False,
+                    'wsclean3': False,
+                    'gain3': False,
+                    'wsclean4': False,
+                    'chgcentre_field': False,
+                    'split': False}
+
+############## Routines
 
 def get_parameters(pfile):
 
@@ -70,7 +87,7 @@ def add_cmd_option(key,parms,defaults):
     return val
 
 
-def generate_WSCLEAN_command(vis,parms):
+def generate_wsclean_command(vis,parms,defaults):
 
     if 'version' in parms.keys():
         cmd = parms['version']
@@ -78,9 +95,15 @@ def generate_WSCLEAN_command(vis,parms):
         cmd = wsclean_version
 
     for key in wsclean_defaults.keys():
-        cmd += add_cmd_option(key,parms,wsclean_defaults)
+        cmd += add_cmd_option(key,parms,defaults)
 
     return cmd+' '+vis
+
+
+def update_defaults(parms,defaults):
+
+    for key in parms.keys():
+        defaults[key] = parms[key]
 
 
 def get_gaincal_option(key,parms,defaults):
@@ -88,29 +111,54 @@ def get_gaincal_option(key,parms,defaults):
     if key in parms.keys():
         val = parms[key]
     else:
-        val = default[key]
-        print("Warning: No {:s} in parms file, using default {:s}".format(key,val))
-
+        val = defaults[key]
+        print('Warning: No {:s} in parms file, using default'.format(key),val)
     return val
 
 
+def init():
+
+    if len(sys.argv)<2:
+        exit("No parameters file given")
+
+    pfile = sys.argv[1]
+    parms = get_parameters(pfile)
+
+    try:  # Check measurement set valid
+        vis = parms['invis']
+    except KeyError:
+        exit("KeyError: missing invis entry in params file")
+    if vis==None:
+        exit('Error: Empty invis entry in parms file')
+    if not os.path.isdir(vis):
+        exit('Error:Measurement set :"{:s}" not found'.format(vis))
+
+    return vis, parms
+
 ############################# Main prog #######################
 
-if len(sys.argv)<2:
-    exit("No parameters file given")
+vis, parms = init()
 
-pfile = sys.argv[1]
-parms = get_parameters(pfile)
+######## Get pipeline list
 
-try:  # Check measurement set valid
-    vis = parms['invis']
-except KeyError:
-    exit("KeyError: missing invis entry in params file")
-if vis==None:
-    exit('Error: Empty invis entry in parms file')
-if not os.path.isdir(vis):
-    exit('Error:Measurement set :"{:s}" not found'.format(vis))
+if 'Pipeline' in parms.keys():  # Check in parms file
+    pipeline = parms['Pipeline']
+    if pipeline==None:
+        exit("Error: Empty Pipeline in parms file")
+else:
+    exit("Error: No Pipeline in parms file")
 
+do_copy = get_gaincal_option('copy',pipeline,pipeline_default)
+do_chgcentre_src= get_gaincal_option('chgcentre_src',pipeline,pipeline_default)
+do_wsclean0 = get_gaincal_option('wsclean0',pipeline,pipeline_default)
+do_gain0 = get_gaincal_option('gain0',pipeline,pipeline_default)
+do_wsclean1 = get_gaincal_option('wsclean1',pipeline,pipeline_default)
+do_gain1 = get_gaincal_option('gain1',pipeline,pipeline_default)
+do_wsclean2 = get_gaincal_option('wsclean2',pipeline,pipeline_default)
+do_gain3 = get_gaincal_option('gain3',pipeline,pipeline_default)
+do_wsclean4 = get_gaincal_option('wsclean4',pipeline,pipeline_default)
+do_chgcentre_field = get_gaincal_option('chgcentre_field',pipeline,pipeline_default)
+do_split = get_gaincal_option('split',pipeline,pipeline_default)
 
 ######## Copy mset to tmpp file for safety
 
@@ -118,15 +166,17 @@ if 'tmpvis' in parms.keys():  # Check in parms file
     tmpvis = parms['tmpvis']
     if tmpvis==None:
         exit("Error: Empty tmpvis in parms file")
-    else:
+else:
         tmpvis = tmpvis_default
 
 if os.path.isdir(tmpvis):  # Make sure doesn't exist
     exit("Error: Temp vis file {:s} already exists".format(tmpvis))
 
 copy_cmd = "cp -R {:s} {:s}".format(vis,tmpvis)
-print(copy_cmd)
-
+if do_copy:
+    print(copy_cmd)
+    status = os.system(copy_cmd)
+    print(status)
 
 ######## Phase rotate to source
 
@@ -144,61 +194,72 @@ RA = src_parms['RA']
 Dec = src_parms['Dec']
 
 chgcen_cmd = "chgcentre {:s} {:s} {:s}".format(tmpvis,RA,Dec)
-print(chgcen_cmd)
-
+if do_chgcentre_src:
+    print(chgcen_cmd)
+    status=os.system(chgcen_cmd)
+    print(status)
 
 ######### WSclean source
 
 try:  # Check wsclean parameters are present
-    ws_parms = parms['WSCLEAN']
+    ws_parms = parms['wsclean0']
 except KeyError:
-    exit("KeyError: missing WSCLEAN entry in params file")
+    exit("KeyError: missing wsclean entry in params file")
 
-ws_cmd = generate_WSCLEAN_command(vis,ws_parms)
-print(ws_cmd)
-
+ws_cmd = generate_wsclean_command(tmpvis,ws_parms,wsclean_defaults)
+if do_wsclean0:
+    print(ws_cmd)
+    status = os.system(ws_cmd)
+    print(status)
 
 ############### Calibration pass
 
-if 'gaincal' in parms.keys():  # Check in parms file
-    cal_parms = parms['gaincal']
+if 'gaincal0' in parms.keys():  # Check in parms file
+    cal_parms = parms['gaincal0']
 else:
-    print("Warning: no gaincal in parms file, using defaults")
+    print("Warning: no gaincal0 in parms file, using defaults")
     cal_parms = cal_parms_default
 
 ##### Make cal_dir
 
 cal_dir = get_gaincal_option('cal_dir',cal_parms,cal_parms_default)
-print(cal_dir)
+#print(cal_dir)
 
 if os.path.isdir(cal_dir):
     exit("Error: Cal_dir {:s} exists already".format(cal_dir)) # Play it safe
 
 mkdir_cmd = "mkdir {:s}".format(cal_dir)
-print(mkdir_cmd)
+if do_gain0:
+    print(mkdir_cmd)
+    status = os.system(mkdir_cmd)
+    print(status)
 
 ###### Touch callib file
 
 callib = get_gaincal_option('callib',cal_parms,cal_parms_default)
 callib = "./{:s}/{:s}".format(cal_dir,callib)
 touch_cmd = "touch {:s}".format(callib)
-print(touch_cmd)
+if do_gain0:
+    print(touch_cmd)
+    status=os.system(touch_cmd)
+    print(status)
 
 ##### Get options
 
-prefix = get_gaincal_option('prefix',cal_parms,cal_parms_default)
-caltable = "./{:s}/{:s}{:d}".format(cal_dir,prefix,0)
+caltable = get_gaincal_option('table',cal_parms,cal_parms_default)
+caltable = "./{:s}/{:s}".format(cal_dir,caltable)
 solint = get_gaincal_option('solint',cal_parms,cal_parms_default)
 minsnr = get_gaincal_option('minsnr',cal_parms,cal_parms_default)
 gaintype = get_gaincal_option('gaintype',cal_parms,cal_parms_default)
 calmode = get_gaincal_option('calmode',cal_parms,cal_parms_default)
+cal_pyfile = get_gaincal_option('script',cal_parms,cal_parms_default)
 
 ###### Generate cal python script
 
 cal_script = "from callibrary import applycaltocallib\n"
 
 cal_line_1 = "gaincal('{:s}', solint='{:s}', docallib=True, caltable='{:s}',\n".format(tmpvis,solint,caltable)
-cal_line_1+= "    callib='{:s}', gaintype='{:s}', calmode='{:s}')\n".format(callib,gaintype,calmode)
+cal_line_1+= "    minsnr={:3.1f},callib='{:s}', gaintype='{:s}', calmode='{:s}')\n".format(minsnr,callib,gaintype,calmode)
 
 cal_line_2 = "applycaltocallib(filename='{:s}', append=True, gaintable='{:s}',\n".format(callib,caltable)
 cal_line_2+= "    calwt=False)\n"
@@ -210,32 +271,41 @@ cal_line_3 = "applycal('{:s}', docallib=True, callib='{:s}')\n".format(tmpvis,ca
 #print(cal_line_2)
 #print(cal_line_3)
 
-cal_pyfile = "{:s}{:d}.py".format(prefix,0)
-print(cal_pyfile)
+if do_gain0:
+    print(cal_pyfile)
 
-if os.path.isfile(cal_pyfile):
-    exit("Error: python file {:s} already exists".format(cal_pyfile))
+    if os.path.isfile(cal_pyfile):
+        exit("Error: python file {:s} already exists".format(cal_pyfile))
 
-cal_out = open(cal_pyfile,'w')
-cal_out.write(cal_script)
-cal_out.write(cal_line_1)
-cal_out.write(cal_line_2)
-cal_out.write(cal_line_3)
-cal_out.close()
+    cal_out = open(cal_pyfile,'w')
+    cal_out.write(cal_script)
+    cal_out.write(cal_line_1)
+    cal_out.write(cal_line_2)
+    cal_out.write(cal_line_3)
+    cal_out.close()
 
 ####### Run gaincal
 
-gain_cal_cmd = "casa --nogui -c {:s}".format(cal_pyfile)
-print(gain_cal_cmd)
-
+    gain_cal_cmd = "casa --nogui -c {:s}".format(cal_pyfile)
+    print(gain_cal_cmd)
+    status = os.system(gain_cal_cmd)
+    print(status)
 
 ####### Run WSclean on new Calibration
 
-ws_parms['data-column'] = 'CORRECTED-DATA'
-ws_parms['name'] = ws_parms['name']+'2'
-ws_cmd = generate_WSCLEAN_command(vis,ws_parms)
-print(ws_cmd)
+try:  # Check wsclean parameters are present
+    ws_parms1 = parms['wsclean1']
+except KeyError:
+    exit("KeyError: missing wsclean entry in params file")
 
+update_defaults(ws_parms1, ws_parms)
+#wsclean_defaults.pop['version'] # Don't want this in defaults
+ws_cmd = generate_wsclean_command(tmpvis,ws_parms,wsclean_defaults)
+
+if do_wsclean1:
+    print(ws_cmd)
+    status = os.system(ws_cmd)
+    print(status)
 
 ####### chgcentre back
 
@@ -245,14 +315,38 @@ if not 'Field' in parms.keys():  # Check in parms file
 phase_centre = parms['Field']['Centre']
 
 chgcen_cmd = "chgcentre {:s} {:s}".format(tmpvis,phase_centre)
-print(chgcen_cmd)
 
+if do_chgcentre_field:
+    print(chgcen_cmd)
+    status = os.system(chgcen_cmd)
+    print(status)
 
 ####### Make split script
 
-outvis = get_gaincal_option('outvis',parms,outvis_default)
-if os.path.isdir(outvis):
-    exit("Error: {:s} already exists".format(outvis))
+if 'Split' in parms.keys():  # Check in parms file
+    split_parms = parms['Split']
+    if split_parms==None:
+        exit("Error: Empty Split in parms file")
+else:
+        split_parms = split_default
 
-split_line = "split(vis={:s}, outputvis={:s}, datacolumn='corrected', width=1, timebin='0s')\n".format(tmpvis,outvis)
-print(split_line)
+if do_split:
+    outvis = get_gaincal_option('outvis',split_parms,split_default)
+    if os.path.isdir(outvis):
+        exit("Error: {:s} already exists".format(outvis))
+
+    split_pyfile = get_gaincal_option('script',split_parms,split_default)
+    if os.path.isfile(split_pyfile):
+        exit("Error: {:s} already exists".format(split_pyfile))
+
+    split_line = "split(vis={:s}, outputvis={:s}, datacolumn='corrected', width=1, timebin='0s')\n".format(tmpvis,outvis)
+#print(split_line)
+
+
+    split_out = open(split_pyfile,'w')
+    split_out.write(split_line)
+    split_out.close()
+    split_cmd = "casa --nogui -c {:s}".format(split_pyfile)
+    print(split_cmd)
+    status = os.system(split_cmd)
+    print(status)
